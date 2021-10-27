63ec4b5e1a108da167d96b0c492c58412a57b0d7
daniel <exodaniel@gmail.com>
2021-10-27 16:49:14 +0200

    better plotting

--- plot_gym_training_progress.py
+++ plot_gym_training_progress.py
@@ -7,8 +7,9 @@ from matplotlib import dates as md
 import matplotlib
 from rich.console import Console
 from rich.table import Table
+from enum import Enum
+import typer
 
-from navrep.tools.commonargs import parse_plotting_args
 from navrep.scripts.plot_gym_training_progress import parse_logfiles, smooth
 
 matplotlib.rcParams['mathtext.fontset'] = 'stix'
@@ -17,10 +18,43 @@ matplotlib.rcParams['font.family'] = 'STIXGeneral'
 MILLION = 1000000
 N_ = 5 # millions of train steps before training is 'complete'
 
-def get_color_and_style(logpath):
-    return None, None
+envname_styles = {
+    "navrep3daltencodedenv": "dashed",
+    "e2enavrep3dtrain": "solid",
+    "navrep3dtrainencodedenv": "solid",
+}
+variant_colors = {
+    "S": "lightskyblue",
+    "Salt": "mediumseagreen",
+    "SC": "khaki",
+    "Random": "brown",
+    "E2E": "grey",
+}
 
-def plot_training_progress(logdirs, scenario="navrep3dtrain", x_axis="total_steps", y_axis="reward"):
+def color_and_style(variant, envname):
+    color = None
+    style = None
+    if variant is not None:
+        color = variant_colors[variant]
+    if envname is not None:
+        style = envname_styles[envname]
+    return color, style
+
+def get_variant(logpath):
+    variant = None
+    string = logpath.split("V64M64_")[-1]
+    string = string.split(".")[0]
+    # find variant in string
+    for k in variant_colors:
+        if string == k:
+            variant = k
+            break
+    return variant
+
+def get_envname(logpath):
+    return os.path.basename(logpath).split("_")[0]
+
+def plot_training_progress(logdirs, scenario=None, x_axis="total_steps", y_axis="reward"):
     logpaths, parents = parse_logfiles(logdirs)
 
     # get set of all scenarios in all logpaths
@@ -31,13 +65,20 @@ def plot_training_progress(logdirs, scenario="navrep3dtrain", x_axis="total_step
         all_scenarios.extend(scenarios)
     all_scenarios = sorted(list(set(all_scenarios)))
 
+    if x_axis == "wall_time":
+        all_scenarios = ["all_scenarios"]
+
+    if scenario is not None:
+        all_scenarios = [scenario]
+
     print()
     print("Plotting scenario rewards")
     print()
-    plt.ion()
     plt.figure("scenario rewards")
     plt.clf()
-    fig, ax = plt.subplots(1, 1, num="scenario rewards")
+    fig, axes = plt.subplots(1, len(all_scenarios), num="scenario rewards")
+    axes = np.array(axes).reshape((-1,))
+    axes = {scenario: ax for ax, scenario in zip(axes, all_scenarios)}
 
     console = Console()
     table = Table(show_header=True, header_style="bold magenta")
@@ -48,81 +89,87 @@ def plot_training_progress(logdirs, scenario="navrep3dtrain", x_axis="total_step
 
     lines = []
     legends = []
-    for logpath, parent in zip(logpaths, parents):
-        logname = os.path.basename(logpath)
-        line = None
-        color, style = get_color_and_style(logpath)
-        S = pd.read_csv(logpath)
-        scenario_S = S[S["scenario"] == scenario]
-        # million steps
-        n = np.max(scenario_S["total_steps"].values) / MILLION
-        # x axis
-        if x_axis == "total_steps":
-            x = scenario_S["total_steps"].values
-            x = x / MILLION
-            xlabel = "Million Train Steps"
-        elif x_axis == "wall_time":
-            try:
-                x = scenario_S["wall_time"].values
-            except KeyError:
-                print("{} has no wall_time info".format(logpath))
-                continue
-            x = md.epoch2num(x)
-            xlabel = "Wall Time"
-        else:
-            raise NotImplementedError
-        if len(x) == 0:
-            continue
-        # y axis
-        if y_axis == "reward":
-            ylabel = "reward"
-            ylim = [-26, 101]
-            rewards = scenario_S["reward"].values
-        elif y_axis == "difficulty":
-            ylabel = "num_walls"
-            ylim = [-1, 22]
-            rewards = scenario_S["num_walls"].values
-        elif y_axis == "progress":
-            ylabel = "total steps [million]"
-            ylim = [-1, 10]
-            rewards = scenario_S["total_steps"].values / MILLION
-        else:
-            raise NotImplementedError
-        y = rewards
-        smooth_y = smooth(y, 0.99)
-        # plot main reward line
-        line, = ax.plot(x, smooth_y, linewidth=1, color=color)
-        color = line.get_c()
-        # add episode reward scatter
-        ax.plot(x, y, color=color, marker=',', linewidth=0, label=scenario)
-        # add vertical line at end of finished runs
-        if x_axis == "wall_time":
-            if n > N_:
-                ax.axvline(x[-1], linestyle=style, linewidth=1, color=color)
+    for scenario in all_scenarios:
+        ax = axes[scenario]
+        for logpath, parent in zip(logpaths, parents):
+            logname = os.path.basename(logpath)
+            line = None
+            variant = get_variant(logpath)
+            envname = get_envname(logpath)
+            color, _ = color_and_style(variant, envname)
+            S = pd.read_csv(logpath)
+            if scenario == "all_scenarios":
+                _, style = color_and_style(variant, envname)
+                scenario_S = S
             else:
-                ax.scatter(x[-1], y[-1], marker='>', facecolor="none", edgecolor=color)
-        ax.set_ylim(ylim)
-        ax.set_ylabel(ylabel)
-        ax.set_xlabel(xlabel)
-        ax.set_title(scenario)
-        if x_axis == "wall_time":
-            xfmt = md.DateFormatter('%d-%b-%Y %H:%M:%S')
-            ax.xaxis.set_major_formatter(xfmt)
-            from matplotlib.ticker import MultipleLocator
-            ax.xaxis.set_minor_locator(MultipleLocator(1))
-            ax.xaxis.set_major_locator(MultipleLocator(3))
-            ax.grid(which='minor', axis='x', linestyle='-')
-            ax.grid(which='major', axis='x', linestyle='-')
-        if line is not None:
-            lines.append(line)
-            legends.append(parent + ": " + logname)
-    # add lucia best hline
-    line = ax.axhline(85., color="red", linewidth=1, alpha=0.5)
-    lines.append(line)
-    legends.append("soadrl")
+                style = None
+                scenario_S = S[S["scenario"] == scenario]
+            if len(scenario_S.values) == 0:
+                continue
+            # million steps
+            n = np.max(scenario_S["total_steps"].values) / MILLION
+            # x axis
+            if x_axis == "total_steps":
+                x = scenario_S["total_steps"].values
+                x = x / MILLION
+                xlabel = "Million Train Steps"
+            elif x_axis == "wall_time":
+                try:
+                    x = scenario_S["wall_time"].values
+                except KeyError:
+                    print("{} has no wall_time info".format(logpath))
+                    continue
+                x = md.epoch2num(x)
+                xlabel = "Wall Time"
+            else:
+                raise NotImplementedError
+            # y axis
+            if y_axis == "reward":
+                ylabel = "reward"
+                ylim = [-26, 101]
+                rewards = scenario_S["reward"].values
+            elif y_axis == "difficulty":
+                ylabel = "num_walls"
+                ylim = [-1, 22]
+                rewards = scenario_S["num_walls"].values
+            elif y_axis == "progress":
+                ylabel = "total steps [million]"
+                ylim = [-1, 10]
+                rewards = scenario_S["total_steps"].values / MILLION
+            else:
+                raise NotImplementedError
+            y = rewards
+            smooth_y = smooth(y, 0.99)
+            # plot main reward line
+            line, = ax.plot(x, smooth_y, linewidth=1, linestyle=style, color=color)
+            color = line.get_c()
+            # add episode reward scatter
+            ax.plot(x, y, color=color, marker=',', linewidth=0, label=scenario)
+            # add vertical line at end of finished runs
+            if x_axis == "wall_time":
+                if n > N_:
+                    ax.axvline(x[-1], linestyle=style, linewidth=1, color=color)
+                else:
+                    ax.scatter(x[-1], y[-1], marker='>', facecolor="none", edgecolor=color)
+            ax.set_ylim(ylim)
+            ax.set_ylabel(ylabel)
+            ax.set_xlabel(xlabel)
+            ax.set_title(scenario)
+            if x_axis == "wall_time":
+                xfmt = md.DateFormatter('%d-%b-%Y %H:%M:%S')
+                ax.xaxis.set_major_formatter(xfmt)
+                from matplotlib.ticker import MultipleLocator
+                ax.xaxis.set_minor_locator(MultipleLocator(1))
+                ax.xaxis.set_major_locator(MultipleLocator(3))
+                ax.grid(which='minor', axis='x', linestyle='-')
+                ax.grid(which='major', axis='x', linestyle='-')
+            if line is not None:
+                lines.append(line)
+                legends.append(parent + ": " + logname)
     # add current time
-    if x_axis == "wall_time":
-        ax.axvline(md.epoch2num(time.time()), color='k', linewidth=1)
+    for ax in axes.values():
+        if x_axis == "wall_time":
+            ax.axvline(md.epoch2num(time.time()), color='k', linewidth=1)
     L = fig.legend(lines, legends, bbox_to_anchor=(1.05, 1.))
     make_legend_pickable(L, lines)
 
@@ -150,20 +197,24 @@ def make_legend_pickable(legend, lines):
         plt.gcf().canvas.draw()
     plt.connect('pick_event', on_click_legenditem)
 
+def str_enum(options: list):
+    return Enum("", {n: n for n in options}, type=str)
+
+def main(logdir="~/navrep3d",
+         x_axis: str_enum(["wall_time", "total_steps"]) = "wall_time", # noqa
+         y_axis: str_enum(["reward", "difficulty", "progress"]) = "difficulty", # noqa (flake8 bug?)
+         refresh: bool = typer.Option(False, help="Updates the plot every minute.")):
+    logdirs = [os.path.expanduser(logdir),]
+    print(x_axis.value)
+    if refresh:
+        while True:
+            plt.ion()
+            plot_training_progress(logdirs, x_axis=x_axis.value, y_axis=y_axis.value)
+            plt.pause(60)
+    else:
+        plot_training_progress(logdirs, x_axis=x_axis.value, y_axis=y_axis.value)
+        plt.show()
+
 
 if __name__ == "__main__":
-    args, _ = parse_plotting_args()
-
-    if args.x_axis is None:
-        args.x_axis = "total_steps"
-    if args.y_axis is None:
-        args.y_axis = "reward"
-
-    while True:
-        logdir = args.logdir
-        if logdir is None:
-            logdir = "~/navrep3d"
-        logdirs = [os.path.expanduser(logdir),]
-        plot_training_progress(logdirs, x_axis=args.x_axis, y_axis=args.y_axis)
-        plt.pause(60)
-
+    typer.run(main)

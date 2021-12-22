4c8b7d0295ed395299ad79a72865d6c5964969fc
daniel <exodaniel@gmail.com>
2021-12-22 20:16:00 +0100

    updates W log plotting

--- plot_W_training_logs.py
+++ plot_W_training_logs.py
@@ -4,33 +4,48 @@ import os
 import numpy as np
 from strictfire import StrictFire
 
+from plot_gym_training_progress import make_legend_pickable
+
 def main(
     logdir="~/navrep3d_W",
+    refresh=False,
 ):
     logdir = os.path.expanduser(logdir)
     logdir = os.path.join(logdir, "logs/W")
 
     plt.close('all')
     while True:
-        plt.figure("training log")
+        fig, ax = plt.subplots(1, 1, num="training log")
         plt.clf()
         logs = sorted(os.listdir(logdir))
-        legend = []
-        lines = []
+        legends = []
+        linegroups = []
         for log in logs:
+            lines = []
             if not log.endswith(".csv"):
                 continue
             path = os.path.join(logdir, log)
-            legend.append(log)
             data = pd.read_csv(path)
             x = data["step"].values
-            y = data["cost"].values
+            y_axis = "lidar_test_error"
+            y = data[y_axis].values
+            y_valid_mask = np.logical_not(np.isnan(y))
+            y = y[y_valid_mask]
+            x = x[y_valid_mask]
+            plt.ylabel(y_axis)
             line, = plt.plot(x, y, label=log)
             plt.axhline(np.min(y), alpha=0.3, linewidth=1, color=line.get_color())
             lines.append(line)
-        plt.legend()   # quick-search : plot tcn training logs
-        plt.ion()
-        plt.pause(10.)
+            linegroups.append(lines)
+            legends.append(log)
+        L = fig.legend([lines[0] for lines in linegroups], legends)
+        make_legend_pickable(L, linegroups)
+        if refresh:
+            plt.ion()
+            plt.pause(10.)
+        else:
+            plt.show()
+            break
 
 
 if __name__ == "__main__":

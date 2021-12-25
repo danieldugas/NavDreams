1875162ea0cb5ee5ee3f2ebb8e787c6ba2f0d5b1
daniel <exodaniel@gmail.com>
2021-12-25 13:52:44 +0100

    calculating n_step cost

--- navrep3d/worldmodel.py
+++ navrep3d/worldmodel.py
@@ -1,3 +1,4 @@
+import copy
 import numpy as np
 import torch
 import torch.nn as nn
@@ -9,6 +10,9 @@ class WorldModel(nn.Module):
     def __init__(self, gpu):
         self.gpu = gpu
         super().__init__()
+
+    def get_block_size():
+        raise NotImplementedError
 
     def forward(self, img, state, action, dones, targets=None, h=None):
         """
@@ -126,3 +130,31 @@ class WorldModel(nn.Module):
         state_pred = state_pred_t.detach().cpu().numpy()
         state_pred = state_pred[0, -1]  # only batch, last item in sequence
         return img_pred, state_pred
+
+def fill_dream_sequence(worldmodel, real_sequence, context_length):
+    """ Fills dream sequence based on context from real_sequence
+        real_sequence is a list of dicts, one for each step in the sequence.
+        each dict has
+        "obs": numpy image (W, H, CH) [0, 1]
+        "state": numpy (2,) [-inf, inf]
+        "action": numpy (3,) [-inf, inf]
+
+        context_length (int): number of steps of the real sequence to keep in the dream sequence
+
+        output:
+        dream_sequence: same length as the real_sequence, but observations and states are predicted
+                open-loop by the worldmodel, while actions are taken from the real sequence
+        """
+    sequence_length = len(real_sequence)
+    dream_sequence = copy.deepcopy(real_sequence[:context_length])
+    dream_sequence[-1]['action'] = None
+    real_actions = [d['action'] for d in real_sequence]
+    next_actions = real_actions[context_length-1:sequence_length-1]
+    for action in next_actions:
+        dream_sequence[-1]['action'] = action * 1.
+        img_npred, goal_pred = worldmodel.get_next(dream_sequence)
+        # update sequence
+        dream_sequence.append(dict(obs=img_npred, state=goal_pred, action=None))
+    dream_sequence[-1]['action'] = next_actions[-1] * 1.
+    return dream_sequence
+
--- test_n_step_dream.py
+++ test_n_step_dream.py
@@ -1,17 +1,56 @@
 import os
+import numpy as np
 from matplotlib import pyplot as plt
 from navrep.tools.wdataset import WorldModelDataset
 from strictfire import StrictFire
+from tqdm import tqdm
 
 from navrep.models.gpt import GPT, GPTConfig, load_checkpoint
 from navrep3d.rssm import RSSMWMConf, RSSMWorldModel
 from navrep3d.tssm import TSSMWMConf, TSSMWorldModel
+from navrep3d.worldmodel import fill_dream_sequence
+
+def worldmodel_n_step_error(worldmodel, test_dataset_folder, sequence_length=32, context_length=16):
+    assert sequence_length <= worldmodel.get_block_size()
+    dream_length = sequence_length - context_length
+    # load dataset
+    seq_loader = WorldModelDataset(test_dataset_folder, sequence_length, lidar_mode="images",
+                                   channel_first=False, as_torch_tensors=False, file_limit=None)
+    seq_loader = tqdm(seq_loader, total=len(seq_loader))
+    obs_error = np.ones((len(seq_loader), dream_length)) * np.nan
+    vecobs_error = np.ones((len(seq_loader), dream_length)) * np.nan
+    for i, (x, a, y, x_rs, y_rs, dones) in enumerate(seq_loader):
+        real_sequence = [dict(obs=x[i], state=x_rs[i], action=a[i]) for i in range(sequence_length)]
+        dream_sequence = fill_dream_sequence(worldmodel, real_sequence, context_length)
+        dream_obs = np.array([d["obs"] for d in dream_sequence[context_length:]]) # (D, W, H, C)
+        dream_vecobs = np.array([d["state"] for d in dream_sequence[context_length:]])
+        real_obs = np.array([d["obs"] for d in real_sequence[context_length:]])
+        real_vecobs = np.array([d["state"] for d in real_sequence[context_length:]])
+        obs_error[i] = np.mean( # mean over all pixels and channels
+            np.reshape(np.square(dream_obs - real_obs), (dream_length, -1)),
+            axis=-1) # now (D,)
+        vecobs_error[i] = np.mean(
+            np.reshape(np.square(dream_vecobs - real_vecobs), (dream_length, -1)),
+            axis=-1) # now (D, )
+        # if a reset is in the sequence, ignore predictions for subsequent frames
+        ignore_error = np.cumsum(dones[context_length:]) > 0 # (D,)
+        obs_error[i, ignore_error] = np.nan
+        vecobs_error[i, ignore_error] = np.nan
+        if i % 100 == 0:
+            seq_loader.set_description(
+                f"1-step error {np.nanmean(obs_error, axis=0)[0]:.1f} \
+                  16-step error {np.nanmean(obs_error, axis=0)[15]:.1f}"
+            )
+    mean_obs_error = np.nanmean(obs_error, axis=0)
+    mean_vecobs_error = np.nanmean(vecobs_error, axis=0)
+    return mean_obs_error, mean_vecobs_error
 
 def main(dataset="SCR",
          gpu=False,
          dream_length=16,
          context_length=16,
          n_examples=5,
+         error=False,
          ):
     sequence_length = dream_length + context_length
 
@@ -22,13 +61,14 @@ def main(dataset="SCR",
                        os.path.expanduser("~/navrep3d_W/datasets/V/navrep3dasl"),
                        os.path.expanduser("~/navrep3d_W/datasets/V/rosbag")]
         examples = [34, 51, 23, 42, 79, 5, 120]
+        examples = [0, 3000, 6000, 9000, 12000, 1000, 4000]
     elif dataset == "staticasl":
         dataset_dir = [os.path.expanduser("~/navrep3d_W/datasets/V/navrep3dasl")]
         examples = [34, 51, 23, 42, 79, 5, 120]
     else:
         raise NotImplementedError(dataset)
 
-    worldmodel_types = ["Transformer", "RSSM_A0", "TSSM_V0"]
+    worldmodel_types = ["Transformer", "RSSM_A0", "TSSM_V1"]
     worldmodels = []
     for worldmodel_type in worldmodel_types:
         if worldmodel_type == "Transformer":
@@ -50,8 +90,8 @@ def main(dataset="SCR",
             model = RSSMWorldModel(mconf, gpu=gpu)
             load_checkpoint(model, wm_model_path, gpu=gpu)
             worldmodel = model
-        elif worldmodel_type == "TSSM_V0":
-            wm_model_path = "~/navrep3d_W/models/W/TSSM_V0_{}".format(dataset)
+        elif worldmodel_type == "TSSM_V1":
+            wm_model_path = "~/navrep3d_W/models/W/TSSM_V1_{}".format(dataset)
             wm_model_path = os.path.expanduser(wm_model_path)
             mconf = TSSMWMConf()
             mconf.image_channels = 3
@@ -60,10 +100,19 @@ def main(dataset="SCR",
             worldmodel = model
         worldmodels.append(worldmodel)
 
+    if error:
+        print("Computing n-step error")
+        for worldmodel in worldmodels:
+            obs_n_step_error, vecobs_n_step_error = worldmodel_n_step_error(
+                worldmodel, dataset_dir, sequence_length=sequence_length, context_length=context_length)
+            plt.plot(obs_n_step_error)
+        plt.show()
+
     example_sequences = {examples[i]: None for i in range(n_examples)}
     seq_loader = WorldModelDataset(dataset_dir, sequence_length, lidar_mode="images",
-                                   channel_first=False, as_torch_tensors=False, file_limit=64)
-    for i, (x, a, y, x_rs, y_rs, dones) in enumerate(seq_loader):
+                                   channel_first=False, as_torch_tensors=False, file_limit=None)
+    print("{} sequences available".format(len(seq_loader)))
+    for i, (x, a, y, x_rs, y_rs, dones) in tqdm(enumerate(seq_loader)):
         if i in example_sequences:
             example_sequences[i] = (x, a, y, x_rs, y_rs, dones)
         if i > max(examples):
@@ -78,16 +127,7 @@ def main(dataset="SCR",
         real_sequence = [dict(obs=x[i], state=x_rs[i], action=a[i]) for i in range(sequence_length)]
         dream_sequences = []
         for worldmodel in worldmodels:
-            dream_sequence = [dict(obs=x[i], state=x_rs[i], action=a[i]) for i in range(context_length)]
-            dream_sequence = dream_sequence[:context_length]
-            dream_sequence[-1]['action'] = None
-            next_actions = a[context_length-1:sequence_length]
-            for action in next_actions:
-                dream_sequence[-1]['action'] = action * 1.
-                img_npred, goal_pred = worldmodel.get_next(dream_sequence)
-                # update sequence
-                dream_sequence.append(dict(obs=img_npred, state=goal_pred, action=None))
-            dream_sequences.append(dream_sequence)
+            dream_sequences.append(fill_dream_sequence(worldmodel, real_sequence, context_length))
 
         for i in range(sequence_length):
             axes[n_rows_per_example*n, i].imshow(real_sequence[i]['obs'])
@@ -99,4 +139,4 @@ def main(dataset="SCR",
 
 
 if __name__ == "__main__":
-    StrictFire(main())
+    StrictFire(main)

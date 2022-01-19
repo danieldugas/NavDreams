5e1efc117fe7d3df7708e6103f6ab5f966917790
daniel <exodaniel@gmail.com>
2022-01-19 11:30:06 +0100

    test_n3d statistics

--- test_n3d.py
+++ test_n3d.py
@@ -1,6 +1,7 @@
 import os
 from strictfire import StrictFire
 from stable_baselines3 import PPO
+from tqdm import tqdm
 
 from navrep3d.navrep3dtrainencodedenv import EncoderObsWrapper
 from navrep3d.navrep3danyenv import NavRep3DAnyEnvDiscrete
@@ -10,6 +11,7 @@ def main(build_name="staticasl", render=True, difficulty_mode="easy"):
     MODELPATH = "/home/daniel/navrep3d/models/gym/navrep3daslencodedenv_2021_12_11__00_23_55_DISCRETE_PPO_GPT_V_ONLY_V64M64_SCR_bestckpt.zip" # noqa
     MODELPATH = "/home/daniel/navrep3d/models/gym/navrep3daslencodedenv_2021_12_08__10_18_09_DISCRETE_PPO_GPT_V_ONLY_V64M64_SCR_bestckpt.zip" # noqa
     MODELPATH = "/home/daniel/navrep3d/models/gym/navrep3dkozehdoldencodedenv_2022_01_13__16_13_45_DISCRETE_PPO_GPT_V_ONLY_V64M64_SCR_bestckpt.zip" # noqa
+    MODELPATH = "/home/daniel/navrep3d/models/gym/navrep3dkozehdencodedenv_2022_01_17__12_55_53_DISCRETE_PPO_GPT_V_ONLY_V64M64_SCR_bestckpt.zip" # noqa # 30%
 
     backend = "GPT"
     encoding = "V_ONLY"
@@ -20,21 +22,30 @@ def main(build_name="staticasl", render=True, difficulty_mode="easy"):
                                  difficulty_mode=difficulty_mode)
     env = EncoderObsWrapper(env, backend=backend, encoding=encoding, variant=variant)
 
-    obs = env.reset()
-    while True:
-        action, _states = model.predict(obs, deterministic=True)
-        obs, reward, done, _ = env.step(action)
-        if done:
-            env.reset()
-            if reward > 50.:
-                print("Success!")
-            else:
-                print("Failure.")
-        if render:
-            if build_name == "rosbag":
-                env.render(save_to_file=True, action_override=action)
-            else:
-                env.render(save_to_file=True)
+    successes = []
+    N = 1000
+    pbar = tqdm(range(N))
+    for i in pbar:
+        obs = env.reset()
+        while True:
+            action, _states = model.predict(obs, deterministic=True)
+            obs, reward, done, _ = env.step(action)
+            if render:
+                if build_name == "rosbag":
+                    env.render(save_to_file=True, action_override=action)
+                else:
+                    env.render(save_to_file=True)
+            if done:
+                if reward > 50.:
+                    if render:
+                        print("Success!")
+                    successes.append(1.)
+                else:
+                    if render:
+                        print("Failure.")
+                    successes.append(0.)
+                pbar.set_description(f"Success rate: {sum(successes)/len(successes):.2f}")
+                break
 
 
 if __name__ == "__main__":

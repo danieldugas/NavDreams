497a3307ff4971f90fd0be1ac9f0961a9bd90363
daniel <exodaniel@gmail.com>
2022-02-16 23:46:58 +0100

    auto_test adds dreamer vs n3d testing

--- auto_test.sh
+++ auto_test.sh
@@ -22,6 +22,7 @@ source ~/cbc3env/bin/activate
 # 2) which models do best on gallery, cathedral, kozehdrs?
 # 3) does a R model trained on SC do well in R?
 # 4) does scope analysis also hold up at test_time?
+# 5) dreamer vs n3d tests in S, R, and K
 
 # 1)
 # S_E2E S
@@ -41,27 +42,58 @@ source ~/cbc3env/bin/activate
 
 # 
     # e2e
-#     "~/navrep3d/models/gym/navrep3daltenv_2021_11_01__08_52_03_DISCRETE_PPO_E2E_VCARCH_C64_ckpt.zip"
+#   "~/navrep3d/models/gym/navrep3daltenv_2021_11_01__08_52_03_DISCRETE_PPO_E2E_VCARCH_C64_ckpt.zip" \
     # n3d
-#     "~/navrep3d/models/gym/navrep3daslencodedenv_2021_12_11__00_23_55_DISCRETE_PPO_GPT_V_ONLY_V64M64_SCR_bestckpt.zip"
-#     "~/navrep3d/models/gym/navrep3daslencodedenv_2021_12_08__10_18_09_DISCRETE_PPO_GPT_V_ONLY_V64M64_SCR_bestckpt.zip"
-#     "~/navrep3d/models/gym/navrep3dSCRencodedenv_2021_12_12__16_46_51_DISCRETE_PPO_GPT_V_ONLY_V64M64_SCR_bestckpt.zip"
-#     "~/navrep3d/models/gym/navrep3dkozehdencodedenv_2022_01_17__12_55_53_DISCRETE_PPO_GPT_V_ONLY_V64M64_SCR_bestckpt.zip"
-#     "~/navrep3d/models/gym/navrep3dkozehdencodedenv_2022_01_17__12_55_53_DISCRETE_PPO_GPT_V_ONLY_V64M64_SCR_ckpt.zip"
-#     "~/navrep3d/models/gym/navrep3dkozehdoldencodedenv_2022_01_13__16_13_45_DISCRETE_PPO_GPT_V_ONLY_V64M64_SCR_bestckpt.zip"
-#     "~/navrep3d/models/gym/navrep3dkozehdoldencodedenv_2022_01_13__16_13_45_DISCRETE_PPO_GPT_V_ONLY_V64M64_SCR_ckpt.zip"
-#     "~/navrep3d/models/gym/navrep3dkozehdremptyencodedenv_2022_01_30__22_12_30_DISCRETE_PPO_GPT_V_ONLY_V64M64_SCRK_bestckpt.zip"
-#     "~/navrep3d/models/gym/navrep3dkozehdremptyencodedenv_2022_01_30__22_12_30_DISCRETE_PPO_GPT_V_ONLY_V64M64_SCRK_ckpt.zip"
-#     "~/navrep3d/models/gym/navrep3daltencodedenv_2021_12_15__08_43_12_DISCRETE_PPO_GPT_V_ONLY_V64M64_SCR_bestckpt.zip"
-#     "~/navrep3d/models/gym/navrep3daslfixedencodedenv_2021_12_29__17_17_16_DISCRETE_PPO_GPT_V_ONLY_V64M64_SCR_bestckpt.zip"
+#   "~/navrep3d/models/gym/navrep3daslencodedenv_2021_12_11__00_23_55_DISCRETE_PPO_GPT_V_ONLY_V64M64_SCR_bestckpt.zip" \
+#   "~/navrep3d/models/gym/navrep3daslencodedenv_2021_12_08__10_18_09_DISCRETE_PPO_GPT_V_ONLY_V64M64_SCR_bestckpt.zip" \
+#   "~/navrep3d/models/gym/navrep3dSCRencodedenv_2021_12_12__16_46_51_DISCRETE_PPO_GPT_V_ONLY_V64M64_SCR_bestckpt.zip" \
+#   "~/navrep3d/models/gym/navrep3dkozehdencodedenv_2022_01_17__12_55_53_DISCRETE_PPO_GPT_V_ONLY_V64M64_SCR_bestckpt.zip" \
+#   "~/navrep3d/models/gym/navrep3dkozehdencodedenv_2022_01_17__12_55_53_DISCRETE_PPO_GPT_V_ONLY_V64M64_SCR_ckpt.zip" \
+#   "~/navrep3d/models/gym/navrep3dkozehdoldencodedenv_2022_01_13__16_13_45_DISCRETE_PPO_GPT_V_ONLY_V64M64_SCR_bestckpt.zip" \
+#   "~/navrep3d/models/gym/navrep3dkozehdoldencodedenv_2022_01_13__16_13_45_DISCRETE_PPO_GPT_V_ONLY_V64M64_SCR_ckpt.zip" \
+#   "~/navrep3d/models/gym/navrep3dkozehdremptyencodedenv_2022_01_30__22_12_30_DISCRETE_PPO_GPT_V_ONLY_V64M64_SCRK_bestckpt.zip" \
+#   "~/navrep3d/models/gym/navrep3dkozehdremptyencodedenv_2022_01_30__22_12_30_DISCRETE_PPO_GPT_V_ONLY_V64M64_SCRK_ckpt.zip" \
+#   "~/navrep3d/models/gym/navrep3daltencodedenv_2021_12_15__08_43_12_DISCRETE_PPO_GPT_V_ONLY_V64M64_SCR_bestckpt.zip" \
+#   "~/navrep3d/models/gym/navrep3daslfixedencodedenv_2021_12_29__17_17_16_DISCRETE_PPO_GPT_V_ONLY_V64M64_SCR_bestckpt.zip" \
     # untested
-#     "~/navrep3d/models/gym/navrep3dkozehdrencodedenv_2022_01_28__06_44_50_DISCRETE_PPO_GPT_V_ONLY_V64M64_K2_ckpt.zip"
-#     "~/navrep3d/models/gym/navrep3dkozehdrencodedenv_2022_01_25__21_34_44_DISCRETE_PPO_GPT_V_ONLY_V64M64_SCRK_ckpt.zip"
-#     "~/navrep3d/models/gym/navrep3dkozehdrencodedenv_2022_01_26__08_56_54_DISCRETE_PPO_GPT_V_ONLY_V64M64_K_ckpt.zip"
-#     "~/navrep3d/models/gym/navrep3dkozehdrsencodedenv_2022_02_02__17_18_59_DISCRETE_PPO_GPT_V_ONLY_V64M64_K2_bestckpt.zip"
+#   "~/navrep3d/models/gym/navrep3dkozehdrencodedenv_2022_01_28__06_44_50_DISCRETE_PPO_GPT_V_ONLY_V64M64_K2_ckpt.zip" \
+#   "~/navrep3d/models/gym/navrep3dkozehdrencodedenv_2022_01_25__21_34_44_DISCRETE_PPO_GPT_V_ONLY_V64M64_SCRK_ckpt.zip" \
+#   "~/navrep3d/models/gym/navrep3dkozehdrencodedenv_2022_01_26__08_56_54_DISCRETE_PPO_GPT_V_ONLY_V64M64_K_ckpt.zip" \
+#   "~/navrep3d/models/gym/navrep3dkozehdrsencodedenv_2022_02_02__17_18_59_DISCRETE_PPO_GPT_V_ONLY_V64M64_K2_bestckpt.zip" \
 # ------------------------------------------------------------------------
 
+# 5)
 cd ~/Code/cbsim/navrep3d
+python test_any.py --model-path \
+  "~/navrep3d/models/gym/navrep3daltencodedenv_2021_12_15__08_43_12_DISCRETE_PPO_GPT_V_ONLY_V64M64_SCR_bestckpt.zip" \
+  --n-episodes 100 --build-name "./alternate.x86_64" --difficulty-mode "hardest" --render False
+python test_any.py --model-path \
+  "~/navrep3d/models/gym/navrep3daslfixedencodedenv_2021_12_29__17_17_16_DISCRETE_PPO_GPT_V_ONLY_V64M64_SCR_bestckpt.zip" \
+  --n-episodes 100 --build-name "staticasl" --difficulty-mode "medium" --render False
+python test_any.py --model-path \
+  "~/navrep3d/models/gym/navrep3dkozehdrsencodedenv_2022_02_02__17_18_59_DISCRETE_PPO_GPT_V_ONLY_V64M64_K2_bestckpt.zip" \
+  --n-episodes 100 --build-name "kozehd" --difficulty-mode "easiest" --render False
+python test_any.py --model-path \
+  "~/navrep3d/models/gym/navrep3dkozehdrsencodedenv_2022_02_02__17_18_59_DISCRETE_PPO_GPT_V_ONLY_V64M64_K2_bestckpt.zip" \
+  --n-episodes 100 --build-name "kozehd" --difficulty-mode "medium" --render False
+cd ~/Code/pydreamer
+python test.py --run-id "f3f47a18b9334a4baa97c728143a00c6" --n-episodes 100 --build-name "./alternate.x86_64" --difficulty-mode "hardest" --render False
+python test.py --run-id "a1ec5269279f46f79af2884526590592" --n-episodes 100 --build-name "staticasl" --difficulty-mode "medium" --render False
+python test.py --run-id "3aaa8d09bce64dd888240a04b714aec7" --n-episodes 100 --build-name "kozehd" --difficulty-mode "easiest" --render False
+python test.py --run-id "3aaa8d09bce64dd888240a04b714aec7" --n-episodes 100 --build-name "kozehd" --difficulty-mode "medium" --render False
+cd ~/Code/cbsim/navrep3d
+python test_any.py --model-path \
+  "~/navrep3d/models/gym/navrep3daltenv_2021_11_15__16_16_40_DISCRETE_PPO_E2E_VCARCH_C64_ckpt.zip" \
+  --n-episodes 100 --build-name "./alternate.x86_64" --difficulty-mode "hardest" --render False
+python test_any.py --model-path \
+  "~/navrep3d/models/gym/navrep3daslfixedencodedenv_2022_01_01__13_09_23_DISCRETE_PPO_E2E_VCARCH_C64_bestckpt.zip" \
+  --n-episodes 100 --build-name "staticasl" --difficulty-mode "medium" --render False
+python test_any.py --model-path \
+  "~/navrep3d/models/gym/navrep3dkozehdrsencodedenv_2022_02_06__22_58_00_DISCRETE_PPO_E2E_VCARCH_C64_bestckpt.zip" \
+  --n-episodes 100 --build-name "kozehd" --difficulty-mode "easiest" --render False
+python test_any.py --model-path \
+  "~/navrep3d/models/gym/navrep3dkozehdrsencodedenv_2022_02_06__22_58_00_DISCRETE_PPO_E2E_VCARCH_C64_bestckpt.zip" \
+  --n-episodes 100 --build-name "kozehd" --difficulty-mode "medium" --render False
 # 2)
 python test_any.py --model-path \
   "~/navrep3d/models/gym/navrep3daltencodedenv_2021_12_15__08_43_12_DISCRETE_PPO_GPT_V_ONLY_V64M64_SCR_ckpt.zip" \

b9106f99cb197cf1a10aee97acee024a66557e38
pepper <exodaniel@gmail.com>
2022-03-22 15:14:43 +0100

    grpcio error message

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -18,6 +18,7 @@ pip install numpy cython # separate because a build dep for some of the next dep
 pip install pyrvo2-danieldugas matplotlib ipython pyyaml snakeviz stable-baselines3  pyglet \
   typer strictfire \
   mlflow \
+  grpcio==1.43.0 \
   navrep navdreams \
   mlagents==0.19.0 \
   torch==1.9.0 \
@@ -26,6 +27,7 @@ pip install pyrvo2-danieldugas matplotlib ipython pyyaml snakeviz stable-baselin
 # tensorflow used by stable baselines (<2), keras by legacy crowd_sim imports
 # jedi because newer versions break ipython (py3.6)
 # gym because of error in (py3.6) https://stackoverflow.com/questions/69520829/openai-gym-attributeerror-module-contextlib-has-no-attribute-nullcontext
+# later versions of grpcio lead to error messages when using mlagents
 pip install mlagents==0.27.0 mlagents-envs==0.27.0 --no-deps # separate because has a too strict pytorch requirement
 
 # install pydreamer and navdreams in development mode
@@ -43,6 +45,12 @@ pip install -e . --no-deps
 git lfs clone git@github.com:ethz-asl/navrep3d_lfs.git ~/navdreams_binaries
 cd ~/navdreams_binaries
 git reset --hard # this shouldn't be necessary, but git-lfs...
+git lfs pull # also shouldn't be necessary ...
+# other option
+# cd ~
+# wget http://robotics.ethz.ch/~asl-datasets/2022_NavDreams/navdreams_binaries_v1.3.tar
+# tar -xvf navdreams_binaries_v1.3.tar
+
 
 # ROS (useful if extracting rosbag)
 # pip install --extra-index-url https://rospypi.github.io/simple/ rospy rosbag tf tf2_ros

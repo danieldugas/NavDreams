5f6f66dd4d312d0a4590fc55261d644a4b444c1e
Ubuntu <exodaniel@gmail.com>
2022-08-28 07:40:24 +0000

    change to train split for recording

--- do.sh
+++ do.sh
@@ -1,5 +1,5 @@
 export CONFIG_FILE=/opt/iGibson/gibson2/examples/configs/locobot_social_nav.yaml
 export TASK=social
-export SPLIT=minival
+export SPLIT=train
 export EPISODE_DIR=/opt/iGibson/gibson2/data/episodes_data/social_nav
 bash submission.sh
